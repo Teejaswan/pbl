0e080d08a187519c025db8ca55d08ba61f318ca7
Teejaswan <teejaswan12@gmail.com>
2022-05-26 21:20:41 +0530

    formatted login page

--- src/components/Choice.svelte
+++ src/components/Choice.svelte
@@ -1,58 +1,57 @@
 <script></script>
- <div>
-      <nav>
-        <div class="Outer" >  
-        <div class="Inner">
-            <button class="Student">Student</button>
-            <button class="Mentor">Mentor</button>
-            <button class="HOD">HOD</button>
-        </div>
-        </div>
-      </nav>
- </div>
-  
+
+<div>
+  <nav>
+    <div class="Outer">
+      <div class="Inner">
+        <button class="Student">Student</button>
+        <button class="Mentor">Mentor</button>
+        <button class="HOD">HOD</button>
+      </div>
+    </div>
+  </nav>
+</div>
+
 <style>
-    button{
-      border: none;
-      outline:none; 
-      background-color : #fff;
-      width: 18vw;
-      height: 4vw;
-      margin-bottom:2em;
-      border-radius:2em;
-      margin-inline: 2vw;
-      box-shadow:0.8vh 0.8vh 0.8vh #cbced1,
-      -0.8vh -0.8vh 0.8vh #fff;
-      transition:top ease 5ms;
-    }
-    button:hover{
-      background-color: #fad9f1; 
-    }
-    button:active{
-      background-color: #fad2ef;
-    }
+  button {
+    border: none;
+    outline: none;
+    background-color: #fff;
+    width: 15vw;
+    height: 4vw;
+    padding: 2px;
+    margin-bottom: 2em;
+    border-radius: 2em;
+    margin-inline: 2em;
+    box-shadow: 0.8vh 0.8vh 0.8vh #cbced1, -0.8vh -0.8vh 0.8vh #fff;
+    transition: all 300ms ease-in-out;
+  }
+  button:hover {
+    box-shadow: 0.8vh 0.8vh 2vh 0.5vh #cbced1, -0.8vh -0.8vh 2vh 0.5vh #fff;
+    background-color: #fad9f1;
+    transform: translate(-2px, -2px);
+  }
+  button:active {
+    background-color: #e491cf;
+    transform: translateY(2px);
+  }
 
-    
-
-    .Inner{
-    
-    display:flex;
-    background-color:#ecf0f3;
-    height:30em;
-    width:25em;
-    justify-content:center;
-    align-items:center;
-    flex-direction:column;
-    border-radius:1em;
-    box-shadow: 0.8em 0.8em 1.25em #cbced1,
-    -0.8em -0.8em 1.25em #fff;
-    }
-    .Outer{
-      display:flex;
-      height: 100vh;
-      background-color: #ecf0f3;
-      justify-content:center;
-      align-items:center;
-    }
-    
+  .Inner {
+    display: flex;
+    background-color: #ecf0f3;
+    height: 25em;
+    width: 25vw;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 1em;
+    box-shadow: 0.8em 0.8em 1.25em #cbced1, -0.8em -0.8em 1.25em #fff;
+  }
+  .Outer {
+    display: flex;
+    height: 100vh;
+    background-color: #ecf0f3;
+    justify-content: center;
+    align-items: center;
+  }
 </style>

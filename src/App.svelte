eac23d0a78105e451029879d27809fe1391fd4d9
Teejaswan <teejaswan12@gmail.com>
2022-05-26 13:37:57 +0530

    added outline and style

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,13 @@
 <script>
+  import Attribute from "./components/attribute.svelte";
   import Header from "./components/Header.svelte";
-  import NavBar from "./components/NavBar.svelte";
+  //import NavBar from "./components/NavBar.svelte";
+  import Steps from "./components/steps.svelte";
+  import Contact from "./components/contact.svelte";
 </script>
 
-<NavBar />
+<!--<NavBar /> -->
 <Header />
+<Steps />
+<Attribute />
+<Contact />
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,26 +3,52 @@
 </script>
 
 <header>
-  <div class="left">
-    <h1>PBL Projects</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-      perferendis autem rem reiciendis provident odio, voluptatem quae ut
-      repudiandae doloribus natus, magnam quis! Ea blanditiis adipisci non fugit
-      ratione eum?
-    </p>
+  <div class="title">
+    <h1>TITLE</h1>
+    <button class="login">login</button>
   </div>
-  <div class="right">
-    <img src={headerBg} alt="fiygbfijyfguy" />
+  <div class="header">
+    <div class="left">
+      <h1>PBL Projects</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+        perferendis autem rem reiciendis provident odio, voluptatem quae ut
+        repudiandae doloribus natus, magnam quis! Ea blanditiis adipisci non
+        fugit ratione eum?
+      </p>
+    </div>
+    <div class="right">
+      <img src={headerBg} alt="fiygbfijyfguy" />
+    </div>
   </div>
 </header>
+<body />
 
 <style>
-  header {
+  .header {
     display: grid;
     grid-template-columns: 1fr 1fr;
     justify-content: center;
     align-items: center;
+    margin-top: 5em;
     margin-inline: 5em;
+    margin-right: 2em;
+    margin-bottom: 10em;
+  }
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: rgb(87, 181, 123);
+    height: 10em;
+    /*border-radius: 0 0 60px 60px;*/
+  }
+  body {
+    all: unset;
+  }
+
+  button {
+    padding: 0;
   }
 </style>

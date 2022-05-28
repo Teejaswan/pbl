7666cde953e37b53d3484b62b967064bd08bc2ff
Gowtham <gowtham2004123@gmail.com>
2022-05-28 12:29:09 +0530

    basic todo layout

--- src/components/todo.svelte
+++ src/components/todo.svelte
@@ -1,9 +1,73 @@
 <script>
+import NavBar from "./NavBar.svelte";
+
 </script>
 
 <header>
   <h1>teams project name</h1>
-</header>
+  <NavBar/>
 
+</header>
+<body>
+<div class="box1">
+
+</div>
+<p style="background-color:white;height:3em;"></p>
+<div class="box2">
+
+</div>
+<p style="background-color:white;height:3em;"></p>
+<div class="box3">
+
+</div>
+<p style="background-color:white;height:3em;"></p>
+</body>
 <style>
+    * {
+    box-sizing: border-box;
+  }
+    .box1{
+        margin: 0;
+    height: 25vh;
+    width: 100vw;
+    overflow: hidden;
+    display: flex;
+    height: 30vh;
+    font-family:"Lato",sans-serif;
+    font-weight:700;
+    color:#555;
+    background-color: rgb(128, 19, 19);
+    justify-content: center;
+    align-items: center;
+    }
+    
+    .box2{
+        margin: 0;
+    height: 25vh;
+    width: 100vw;
+    overflow: hidden;
+    display: flex;
+    height: 30vh;
+    font-family:"Lato",sans-serif;
+    font-weight:700;
+    color:#555;
+    background-color: rgb(128, 19, 19);
+    justify-content: center;
+    align-items: center;
+    }
+    .box3{
+        margin: 0;
+    height: 25vh;
+    width: 100vw;
+    overflow: hidden;
+    display: flex;
+    height: 30vh;
+    font-family:"Lato",sans-serif;
+    font-weight:700;
+    color:#555;
+    background-color: rgb(128, 19, 19);
+    justify-content: center;
+    align-items: center;
+    }
+
 </style>

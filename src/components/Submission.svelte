4f7218cba454391714a7469f6426c8ec2e0371f6
Nivashini <nivashinias@gmail.com>
2022-05-29 22:15:28 +0530

    Modified submission page

--- src/components/Submission.svelte
+++ src/components/Submission.svelte
@@ -1,18 +1,59 @@
 <script>
     import NavBar from "./NavBar.svelte";
 </script>
-
-<div class="container">
-    <div class="header">
-        <h2>Teams Project Name</h2>
-        <h4>Idea Submission</h4>
+<body>
+    <div class="head">
+        <h2>TEAM NAME</h2>
     </div>
     <div class="Add">
-     <h1> Quote 2 </h1>
-     <svg class="svg-icon" viewBox="0 0 20 20">
-        <path fill="none" d="M13.388,9.624h-3.011v-3.01c0-0.208-0.168-0.377-0.376-0.377S9.624,6.405,9.624,6.613v3.01H6.613c-0.208,0-0.376,0.168-0.376,0.376s0.168,0.376,0.376,0.376h3.011v3.01c0,0.208,0.168,0.378,0.376,0.378s0.376-0.17,0.376-0.378v-3.01h3.011c0.207,0,0.377-0.168,0.377-0.376S13.595,9.624,13.388,9.624z M10,1.344c-4.781,0-8.656,3.875-8.656,8.656c0,4.781,3.875,8.656,8.656,8.656c4.781,0,8.656-3.875,8.656-8.656C18.656,5.219,14.781,1.344,10,1.344z M10,17.903c-4.365,0-7.904-3.538-7.904-7.903S5.635,2.096,10,2.096S17.903,5.635,17.903,10S14.365,17.903,10,17.903z"></path>
-     </svg>
+        <h1> Quote 2 </h1>
+       </div>
+    <div class="container">
+      <div class="statement">
+        <h3>PROBLEM STATEMENT</h3>  
+        <input type="text" placeholder="Enter statement "/>
     </div>
-
-
+    <div class="idea">
+        <h3>SOLUTION</h3>
+        <input type="text" placeholder="Enter solution "/>
+    </div>
+    <div class="team">
+        <h4>TEAM</h4>
+        <input type="text" placeholder="Team number and Posted by"/>
+    </div>
 </div>
+
+
+</body>
+<style>
+    .head{
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    background-color: #d36767;
+    height: 8 em;
+    margin-bottom: 5em;
+    } 
+    .statement,.idea,.team{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        border:1px solid black;
+        width:auto;
+    }
+    .statement input{
+        margin-left:4.7vh;
+        width:50%;
+
+    }
+    .idea input{
+        margin-left: 25vh;
+        width:50%;
+    }
+    .team input{
+        margin-left: 34vh;
+        width:50%
+    }
+   
+</style>

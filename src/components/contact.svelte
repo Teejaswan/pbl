4d4efa602305a703468dcfa4b4837ca97e3972f3
Nivashini <nivashinias@gmail.com>
2022-06-30 12:13:36 +0530

    Changes made in contact page

--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -3,39 +3,61 @@
 </script>
 
 <div class="container">
-  <h3 class="outer">Contact</h3>
-  <div class="inner">
-    <div>
-      <h4>Email</h4>
-    </div>
-    <div>
-      <h4>phone number</h4>
-      <h5>College: 0422-2661100</h5>
-      <h5>Principal: 0422-2661121</h5>
-    </div>
+  <div class="outer1">
+  <div class="outer">
+    <div class="inner">
+      <h3>Contact</h3>
+      </div>
+      </div>
+      </div>
+      <div class="contact">
+        <h4>0422-1234567</h4>
+        <br>
+        <a href="https://www.kct.ac.in/">www.kct.ac.in</a><br>
+     </div>
+     <div class="media">
+         <a href="https://www.kct.ac.in/">www.kct.ac.in</a><br>
+         <a href="https://www.kct.ac.in/">www.kct.ac.in</a><br>
+         <a href="https://www.kct.ac.in/">www.kct.ac.in</a><br>
+     </div>
+
+     
     <!--<div>
       <h4>Websites</h4>
       <a href="https://www.kct.ac.in/">KCT</a><br>
       <a href="https://kciri.kct.ac.in/projects/">Projects</a><br>
       <a href="https://blog.kct.ac.in/pbl-students-paper-presentation/">Presentations</a><br>
     </div>-->
-  </div>
 </div>
 
 <style>
   .inner {
-    display: flex;
-    justify-content: space-around;
+    clip-path: polygon(0 0, 100% 0%, 80% 100%, 0% 100%);
+    background-color: #6273CB;
+    width:15vw;
+
   }
   .outer {
-    padding: 2em;
-    text-align: left;
-    
+    clip-path: polygon(0 0, 100% 0%, 80% 100%, 0% 100%);
+    background-color: #E1FF27;
+    width:17vw;
   }
-  h4 {
-    padding-left: 25px;
+  .outer1{
+    margin:2em 0 0 2em;
   }
   .container {
     background-color: #E9ECFD;
+  }
+  h3{
+    text-align:left;
+    justify-content: space-around;
+    padding: 0.5em 0.25em 0.5em 0.25em;
+    font-size: calc(0.85em + 0.4vmin);
+  }
+  .contact{
+    padding:2em 0 2em 3em;
+  }
+  .media{
+  
   }
 </style>

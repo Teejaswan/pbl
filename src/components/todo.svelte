4d458d103d868145552c8db464f5f7bb0bc7748e
Gowtham <gowtham2004123@gmail.com>
2022-07-07 15:31:42 +0530

    todo layout

--- src/components/todo.svelte
+++ src/components/todo.svelte
@@ -1,11 +1,11 @@
 <script>
-  import NavBar from "./NavBar.svelte";
+ 
 
   const user = JSON.parse(localStorage.getItem("user"));
   const team = JSON.parse(localStorage.getItem("team"));
 </script>
 
-<div class="container">
+<!--<div class="container">
   <div class="header">
     <h2>Teams {team.no}</h2>
     <NavBar />
@@ -203,4 +203,290 @@
     justify-content: center;
     align-items: center;
   }
+</style> -->
+
+<nav>
+  <div class="title"></div>
+  <div class="content">
+    <h3>PROJECT NAME</h3>
+    <h4><a href="/steps">Home</a></h4>
+    <h4><a href="/attribute">Profile</a></h4>
+    <h4><a href="/contact">Post</a></h4>
+    <h4><a href="/progress">Progress</a></h4>
+    </div>
+</nav>
+<div class="main">
+  <div class="task_to_do">
+  	<div class="ttd">
+      
+    	<div class="t1">
+       <div class="t11">
+         <div class="left">
+           <div class="up"></div>
+           <div class="down">
+             <div class="d1"></div>
+             <div class="d2"></div>
+           </div>
+         </div>
+         <div class="right">
+           <div class="upr">hi</div>
+           <div class="downr">hello</div>
+         </div>
+       </div> 
+      </div>
+    	<div class="t2">
+       
+      </div>
+    </div>
+  </div>
+  <div class="over_due">
+  	<div class="od">
+    	<div class="o1"></div>
+    	<div class="o2"></div>
+  	</div>
+  </div>
+  <div class="completed">
+  	<div class="c">
+    	<div class="c1"></div>
+    	<div class="c2"></div>
+  	</div> 
+</div>
+</div>
+<style>
+	.title{
+    width: 100vw;
+    background-color:#E1FF27;
+    display: flex;
+    flex-direction:row;
+    justify-content: space-around;
+    align-items: center;
+    margin: 0;
+    height: 20vh;
+    top:-3vh;
+    position:absolute;
+    clip-path: polygon(0 1%, 100% 0, 100% 100%, 0 75%);
+    z-index:-2; 
+    /*border-radius: 0 0 60px 60px;*/
+  }
+  .content{
+   display: flex;
+   flex-direction:row;
+   justify-content: space-around;
+   align-items: center;
+   width:100vw;
+   height: 13vh; 
+   font-size: calc(1vh + 0.5vmin);
+  }
+  .main{
+    display:flex;
+    justify-content: space-around;
+    align-items: center;
+    width:90%;
+    height:80%;
+    flex-wrap: wrap;
+    margin-top: 2.5rem;
+    margin-right: 3rem;
+    margin-left: 3rem;
+    
+  }
+  .task_to_do{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height:90%;
+    width:30%;
+    background-color: #6273CB;
+    flex-wrap: wrap;
+    box-shadow: 0.8em 0.8em 2em -0.3em rgb(189, 188, 188);
+    color: white;
+  }
+  .ttd{
+  	display:flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    height:87.5%;
+    width:100%;
+    background-color: white;
+    flex-wrap: wrap;	
+    margin-top: 3.5rem;
+  }
+  .t1{
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    width:80%;
+    height: 40%;
+    background-color: #E1FF27;
+    border-radius: 1rem;
+  }
+  .t11{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height:87.5%;
+    width:100%;
+    background-color: red;
+    flex-wrap: wrap;  
+    margin-bottom: 3.5rem;
+  }
+  .left{
+    display: flex;
+    align-items:center ;
+    justify-content: space-evenly;
+    width: 60%;
+    height: 100%;
+    background-color: beige;
+    flex-direction: column;
+  }
+ 
+  .up{
+    display: flex;
+    align-items:center ;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 30%;
+    background-color: orange;
+  }
+  .down{
+    display: flex;
+    align-items:center ;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 70%;
+    background-color: blue;
+  }
+  .d1{
+    display: flex;
+    align-items:center ;
+    justify-content: space-evenly;
+    width: 50%;
+    height: 100%;
+    background-color: blue;
+  }
+  .d2{
+    display: flex;
+    align-items:center ;
+    justify-content: space-evenly;
+    width: 50%;
+    height: 100%;
+    background-color: white;
+  }
+   .right{
+    display: flex;
+    align-items:center ;
+    justify-content: space-evenly;
+    width: 40%;
+    height: 100%;
+    background-color: violet;
+    flex-direction: column;
+  }
+  .upr{
+    display: flex;
+    align-items:center ;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 50%;
+    background-color: grey;
+  }
+  .downr{
+    display: flex;
+    align-items:center ;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 50%;
+    background-color: grey;
+  }
+ 
+  .t2{
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    width:80%;
+    height: 40%;
+    background-color: aquamarine;
+    border-radius: 1rem;
+  }
+  .over_due{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height:90%;
+    width:30%;
+    background-color: #6273CB;
+    flex-wrap: wrap;
+    box-shadow: 0.8em 0.8em 2em -0.3em rgb(189, 188, 188);
+  }
+  .od{
+    display:flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    height:87.5%;
+    width:100%;
+    background-color: white;
+    flex-wrap: wrap;  
+    margin-top: 3.5rem;
+  }
+  .o1{
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    width:80%;
+    height: 40%;
+    background-color: aquamarine;
+    border-radius: 1rem;
+  }
+  .o2{
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    width:80%;
+    height: 40%;
+    background-color: aquamarine;
+    border-radius: 1rem;
+  }
+  .completed{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height:90%;
+    width:30%;
+    background-color: #6273CB;
+    flex-wrap: wrap;
+    box-shadow: 0.8em 0.8em 2em -0.3em rgb(189, 188, 188);
+  }
+  .c{
+    display:flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    height:87.5%;
+    width:100%;
+    background-color: white;
+    flex-wrap: wrap;  
+    margin-top: 3.5rem;
+  }
+  .c1{
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    width:80%;
+    height: 40%;
+    background-color: aquamarine;
+    border-radius: 1rem;
+  }
+  .c2{
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    width:80%;
+    height: 40%;
+    background-color: aquamarine;
+    border-radius: 1rem;
+  }
+    
 </style>
+

bc97fb5c1a0073c3f659acc021d8023a75e4b328
gowtham <gowtham2004123@gmail.com>
2022-07-16 09:42:06 +0530

    todo layout start

--- src/components/todo.svelte
+++ src/components/todo.svelte
@@ -1,14 +1,23 @@
 <script>
+import NavBar from "./NavBar.svelte";
+let title = "Worksapce";
+let links = [{name:"Home",link:"/home"},
+             {name:"Profile",link:"/profile"},
+             {name:"Posts",link:"/posts"},
+             {name:"progress",link:"/user/student/progress"}
+            ];
+
+
  
 
   const user = JSON.parse(localStorage.getItem("user"));
   const team = JSON.parse(localStorage.getItem("team"));
 </script>
-
-<!--<div class="container">
+<NavBar {title} {links}/>
+<!-- <div class="container">
   <div class="header">
     <h2>Teams {team.no}</h2>
-    <NavBar />
+    
   </div>
   <h1>Quote-1</h1>
   <hr />
@@ -203,290 +212,269 @@
     justify-content: center;
     align-items: center;
   }
-</style> -->
-
-<nav>
-  <div class="title"></div>
-  <div class="content">
-    <h3>PROJECT NAME</h3>
-    <h4><a href="/steps">Home</a></h4>
-    <h4><a href="/attribute">Profile</a></h4>
-    <h4><a href="/contact">Post</a></h4>
-    <h4><a href="/progress">Progress</a></h4>
+</style>  -->
+<body>
+  <div class="bodytodo">
+    <div class="work">
+      <p style="font-size:1.5rem;"><b>WORKSPACE</b></p>
     </div>
-</nav>
-<div class="main">
-  <div class="task_to_do">
-  	<div class="ttd">
-      
-    	<div class="t1">
-       <div class="t11">
-         <div class="left">
-           <div class="up"></div>
-           <div class="down">
-             <div class="d1"></div>
-             <div class="d2"></div>
-           </div>
-         </div>
-         <div class="right">
-           <div class="upr">hi</div>
-           <div class="downr">hello</div>
-         </div>
-       </div> 
+    <div class="content">
+      <div class="c1">
+        <div class="todo">
+          <div class="head">
+            <p><b>TASK TO DO</b></p>
+          </div>
+          <div class="inner">
+            <div class="up">
+              <div class="left">
+                <div class="left1">
+                  <p style="font-size:1.5rem;"><b>Completing the UI</b></p>
+                </div>
+                <div class="left2">
+                  <div class="left21">
+                    <p style="color:rgba(0, 0, 0, 0.5);font-size:1.2rem;">Assigned Date</p>
+                    <p><b>12-06-2022</b></p>
+                  </div>
+                  <div class="left22">
+                    <p style="color:rgba(0, 0, 0, 0.5);font-size:1.2rem;">Due Date</p>
+                    <p><b>12-07-2022</b></p>
+                  </div>
+                </div>
+              </div>
+              <div class="right">
+                <div class="right1">
+                  <p style="color:rgba(0, 0, 0, 0.5);">Assigner</p>
+                  <p style="font-size:1.2rem;"><b>MENTOR</b></p>
+                </div>
+                <div class="right2">
+                  <p style="color:rgba(0, 0, 0, 0.5);">Assignee</p>
+                  <p style="font-size:1.2rem;"><b>MEMBER-1</b></p>
+                </div>
+              </div>
+            </div>
+            <div class="bottom"></div>
+          </div>
+        </div>
       </div>
-    	<div class="t2">
-       
+      <div class="c2">
+        <div class="todo">
+          <div class="head">
+            <p><b>OVER-DUE</b></p>
+          </div>
+          <div class="inner">
+            <div class="up">
+              <div class="left">
+                <div class="left1">
+                  <p style="font-size:1.5rem;"><b>Completing the UI</b></p>
+                </div>
+                <div class="left2">
+                  <div class="left21">
+                    <p style="color:rgba(0, 0, 0, 0.5);font-size:1.2rem;">Assigned Date</p>
+                    <p><b>12-06-2022</b></p>
+                  </div>
+                  <div class="left22">
+                    <p style="color:rgba(0, 0, 0, 0.5);font-size:1.2rem;">Due Date</p>
+                    <p><b>12-07-2022</b></p>
+                  </div>
+                </div>
+              </div>
+              <div class="right">
+                <div class="right1">
+                  <p style="color:rgba(0, 0, 0, 0.5);">Assigner</p>
+                  <p style="font-size:1.2rem;"><b>MENTOR</b></p>
+                </div>
+                <div class="right2">
+                  <p style="color:rgba(0, 0, 0, 0.5);">Assignee</p>
+                  <p style="font-size:1.2rem;"><b>MEMBER-1</b></p>
+                </div>
+              </div>
+            </div>
+            <div class="bottom"></div>
+          </div>
+        </div>
+      </div>
+      <div class="c3">
+        <div class="todo">
+          <div class="head">
+            <p><b>COMPLETED</b></p>
+          </div>
+          <div class="innercom">
+            <div class="up">
+              <div class="left">
+                <div class="left1">
+                  <p style="font-size:1.5rem;"><b>Completing the UI</b></p>
+                </div>
+                <div class="left2">
+                  <div class="left21">
+                    <p style="color:rgba(0, 0, 0, 0.5);font-size:1.2rem;">Assigned Date</p>
+                    <p><b>12-06-2022</b></p>
+                  </div>
+                  <div class="left22">
+                    <p style="color:rgba(0, 0, 0, 0.5);font-size:1.2rem;">Due Date</p>
+                    <p><b>12-07-2022</b></p>
+                  </div>
+                </div>
+              </div>
+              <div class="right">
+                <div class="right1">
+                  <p style="color:rgba(0, 0, 0, 0.5);">Assigner</p>
+                  <p style="font-size:1.2rem;"><b>MENTOR</b></p>
+                </div>
+                <div class="right2">
+                  <p style="color:rgba(0, 0, 0, 0.5);">Assignee</p>
+                  <p style="font-size:1.2rem;"><b>MEMBER-1</b></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-  <div class="over_due">
-  	<div class="od">
-    	<div class="o1"></div>
-    	<div class="o2"></div>
-  	</div>
-  </div>
-  <div class="completed">
-  	<div class="c">
-    	<div class="c1"></div>
-    	<div class="c2"></div>
-  	</div> 
-</div>
-</div>
+</body>
 <style>
-	.title{
-    width: 100vw;
-    background-color:#E1FF27;
-    display: flex;
-    flex-direction:row;
-    justify-content: space-around;
-    align-items: center;
-    margin: 0;
-    height: 20vh;
-    top:-3vh;
-    position:absolute;
-    clip-path: polygon(0 1%, 100% 0, 100% 100%, 0 75%);
-    z-index:-2; 
-    /*border-radius: 0 0 60px 60px;*/
-  }
-  .content{
-   display: flex;
-   flex-direction:row;
-   justify-content: space-around;
-   align-items: center;
-   width:100vw;
-   height: 13vh; 
-   font-size: calc(1vh + 0.5vmin);
-  }
-  .main{
-    display:flex;
-    justify-content: space-around;
-    align-items: center;
-    width:90%;
-    height:80%;
-    flex-wrap: wrap;
-    margin-top: 2.5rem;
-    margin-right: 3rem;
-    margin-left: 3rem;
-    
-  }
-  .task_to_do{
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    height:90%;
-    width:30%;
-    background-color: #6273CB;
-    flex-wrap: wrap;
-    box-shadow: 0.8em 0.8em 2em -0.3em rgb(189, 188, 188);
-    color: white;
-  }
-  .ttd{
-  	display:flex;
-    justify-content: space-evenly;
-    align-items: center;
-    flex-direction: column;
-    height:87.5%;
-    width:100%;
-    background-color: white;
-    flex-wrap: wrap;	
-    margin-top: 3.5rem;
-  }
-  .t1{
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    width:80%;
-    height: 40%;
-    background-color: #E1FF27;
-    border-radius: 1rem;
-  }
-  .t11{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    height:87.5%;
-    width:100%;
-    background-color: red;
-    flex-wrap: wrap;  
-    margin-bottom: 3.5rem;
-  }
-  .left{
-    display: flex;
-    align-items:center ;
-    justify-content: space-evenly;
-    width: 60%;
-    height: 100%;
-    background-color: beige;
-    flex-direction: column;
-  }
- 
-  .up{
-    display: flex;
-    align-items:center ;
-    justify-content: space-evenly;
-    width: 100%;
-    height: 30%;
-    background-color: orange;
-  }
-  .down{
-    display: flex;
-    align-items:center ;
-    justify-content: space-evenly;
-    width: 100%;
-    height: 70%;
-    background-color: blue;
-  }
-  .d1{
-    display: flex;
-    align-items:center ;
-    justify-content: space-evenly;
-    width: 50%;
-    height: 100%;
-    background-color: blue;
-  }
-  .d2{
-    display: flex;
-    align-items:center ;
-    justify-content: space-evenly;
-    width: 50%;
-    height: 100%;
-    background-color: white;
-  }
-   .right{
-    display: flex;
-    align-items:center ;
-    justify-content: space-evenly;
-    width: 40%;
-    height: 100%;
-    background-color: violet;
-    flex-direction: column;
-  }
-  .upr{
-    display: flex;
-    align-items:center ;
-    justify-content: space-evenly;
-    width: 100%;
-    height: 50%;
-    background-color: grey;
-  }
-  .downr{
-    display: flex;
-    align-items:center ;
-    justify-content: space-evenly;
-    width: 100%;
-    height: 50%;
-    background-color: grey;
-  }
- 
-  .t2{
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    width:80%;
-    height: 40%;
-    background-color: aquamarine;
-    border-radius: 1rem;
-  }
-  .over_due{
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    height:90%;
-    width:30%;
-    background-color: #6273CB;
-    flex-wrap: wrap;
-    box-shadow: 0.8em 0.8em 2em -0.3em rgb(189, 188, 188);
-  }
-  .od{
-    display:flex;
-    justify-content: space-evenly;
-    align-items: center;
-    flex-direction: column;
-    height:87.5%;
-    width:100%;
-    background-color: white;
-    flex-wrap: wrap;  
-    margin-top: 3.5rem;
-  }
-  .o1{
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    width:80%;
-    height: 40%;
-    background-color: aquamarine;
-    border-radius: 1rem;
-  }
-  .o2{
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    width:80%;
-    height: 40%;
-    background-color: aquamarine;
-    border-radius: 1rem;
-  }
-  .completed{
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    height:90%;
-    width:30%;
-    background-color: #6273CB;
-    flex-wrap: wrap;
-    box-shadow: 0.8em 0.8em 2em -0.3em rgb(189, 188, 188);
-  }
-  .c{
-    display:flex;
-    justify-content: space-evenly;
-    align-items: center;
-    flex-direction: column;
-    height:87.5%;
-    width:100%;
-    background-color: white;
-    flex-wrap: wrap;  
-    margin-top: 3.5rem;
-  }
-  .c1{
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    width:80%;
-    height: 40%;
-    background-color: aquamarine;
-    border-radius: 1rem;
-  }
-  .c2{
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    width:80%;
-    height: 40%;
-    background-color: aquamarine;
-    border-radius: 1rem;
-  }
-    
+.bodytodo{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width:96rem;
+  height:32rem;
+}
+.work{
+  display:flex;
+  width:90rem;
+}
+.content{
+  display:flex;
+  width:90rem;
+}
+.c1{
+  display:flex;
+  align-items: center;
+  width:30rem;
+  padding: auto;
+}
+.c2{
+  display:flex;
+  align-items: center;
+  width:30rem;
+}
+.c3{
+  display:flex;
+  align-items: center;
+  width:30rem;
+}
+.todo{
+  display:flex;
+  flex-direction: column;
+  width:28rem;
+  height:26.5rem;
+  box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.25);
+}
+.head{
+  display:flex;
+  height:3rem;
+  background-color: #6273CB;
+  align-items: center;
+  justify-content: center;
+  color:white;
+}
+.inner{
+  display: flex;
+  flex-direction: column;
+  width:90%;
+  height:70%;
+  background-color:  #E1FF27;
+  margin-left: 1.3rem;
+  margin-top: 1.5rem;
+  border-radius:2rem;
+}
+.up{
+  display:flex;
+  height:80%;
+  background-color: #E9ECFD;
+  border-radius:2rem;
+}
+.left{
+  display: flex;
+  flex-direction: column;
+  width:70%;
+  height:100%;
+}
+.left1{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width:100%;
+  height: 40%;
+}
+.left2{
+  display: flex;
+  width:100%;
+  height:60%;
+}
+.left21{
+  display: flex;
+  flex-direction: column;
+  width:50%;
+  height:100%;
+  align-items: center;
+  justify-content:center;
+}
+.left22{
+  display: flex;
+  flex-direction: column;
+  width:50%;
+  height:100%;
+  align-items: center;
+  justify-content:center;
+}
+.right{
+  display: flex;
+  flex-direction: column;
+  width:30%;
+  height:100%;
+}
+.right1{
+  display:flex;
+  flex-direction: column;
+  width:100%;
+  height:50%;
+  justify-content: center;
+  align-items: center;
+}
+.right2{
+  display:flex;
+  flex-direction: column;
+  width:100%;
+  height:50%;
+  justify-content: center;
+  align-items: center;
+}
+.bottom{
+  display:flex;
+  background-color: #E1FF27;
+  height:20%;
+  border-radius:0 0 2rem 2rem;
+}
+.innercom{
+  display: flex;
+  flex-direction: column;
+  width:90%;
+  height:70%;
+  margin-left: 1.3rem;
+  margin-top: 1.5rem;
+  border-radius:2rem;
+}
+@media only screen and (max-width: 1000px) {
+        .content{
+        display: flex;
+        flex-direction: column;
+        height:40rem;
+        width:100%;
+        position: relative;
+        margin-left:3.5rem;
+        }
+    }  
 </style>
 

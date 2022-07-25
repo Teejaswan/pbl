eb4bdce1691ad8a00713eb1f1f79c3a245aa37f2
Nivashini <nivashinias@gmail.com>
2022-07-25 23:23:55 +0530

    Made changes in Home and Mentor page

--- src/components/Mentor.svelte
+++ src/components/Mentor.svelte
@@ -1,15 +1,8 @@
 <script>
-/*import App from "src/App.svelte";*/
-
   import Icon from "./icon.svelte";
   import NavBar from "./NavBar.svelte";
-  
   const user = JSON.parse(localStorage.getItem("user"));
-  /*const team=[{
-    team:Team1;
-  }]*/
 </script>
-
 <div class="container">
    <div class="head">
     <!-- <h1>{user.name}</h1> -->
@@ -72,7 +65,7 @@
           <div class="number">
             <h3>TEAM 1</h3>
             </div>
-            <div class="links">
+            <div class="links" id="trans">
               <a href="GO TO PROFILE">GO TO PROFILE</a>
             </div>
         </div>
@@ -80,7 +73,7 @@
           <div class="number">
             <h3>TEAM 2</h3>
             </div>
-            <div class="links">
+            <div class="links" id="trans">
               <a href="GO TO PROFILE">GO TO PROFILE</a>
             </div>
         </div>
@@ -88,14 +81,13 @@
           <div class="number">
             <h3>TEAM 3</h3>
             </div>
-            <div class="links">
+            <div class="links" id="trans">
               <a href="GO TO PROFILE">GO TO PROFILE</a>
             </div>
         </div>
     </div>
     </div>
-    
- <div class="count">
+  <div class="count">
   <div class="content">
     <div class="id">
         <h3>TEAM 1</h3>
@@ -215,140 +207,6 @@
    </div>
   </div>
 </div>
-
-
-<!--<div class="profile">
-  <div class="profile1">
-  <div class="teams">
-  </div>
-  <div class="name">
-   <div class="name1" >
-    <h3>NAME:</h3>
-   </div>
-   <div class="name2">
-    <h4>Mentor1</h4>
-   </div>
-  </div>
-  <div class="email">
-    <div class="email1">
-      <h3>EMAIL:</h3>
-    </div>
-    <div class="email2">
-      <h4>mentor@gmail.com</h4>
-    </div>
-  </div>
-  <div class="phone">
-    <div class="phone1">
-      <h3>MOBILE:</h3>
-    </div>
-    <div class="phone2">
-       <h4>1234567890</h4>
-    </div>
-  </div>
-  <div class="info">
-    <div class="info1">
-      <h3>INFO:</h3>
-    </div>
-    <div class="info2">
-      <h4>Information</h4>
-    </div>
-  </div>
-  </div>
-  <div class="choice">
-    <div class="myteam">
-      <h3>MY TEAMS</h3>
-    </div>
-    <div class="teamnumber">
-      <div class="team1">
-        <div class="number">
-          <h3>TEAM 1</h3>
-          </div>
-          <div class="links">
-            <a href="GO TO PROFILE">GO TO PROFILE</a>
-          </div>
-       </div>
-      <div class="team2">
-        <div class="number">
-          <h3>TEAM 2</h3>
-          </div>
-          <div class="links">
-            <a href="GO TO PROFILE">GO TO PROFILE</a>
-          </div>
-        </div>
-       <div class="team3">
-        <div class="number">
-          <h3>TEAM 3</h3>
-          </div>
-          <div class="links">
-            <a href="GO TO PROFILE">GO TO PROFILE</a>
-          </div>
-        </div>
-    </div>
-  </div>
-  </div>
-  </div>-->
-
-
-
-
-
-  <!--<div class="myteams">
-    <h1>My Teams</h1>
-    <div class="outer1">
-      <div class="inner1">
-        <div class="front1">
-          <h3>Team number</h3>
-        </div>
-        <div class="below1">
-          <div class="options1">
-            <a href=" ">Go to profile</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  
-  </div>
-  <div class="teams">
-    <div class="header2">
-      <h1>Available teams</h1>
-      <div class="icon">
-        <Icon icon="add" />
-      </div>
-    </div>
-    <div class="outer2">
-      <div class="inner2">
-        <div class="front2">
-          <div class="left">
-            <h5>Team member-1</h5>
-            <h5>Team member-2</h5>
-            <h5>Team member-3</h5>
-            <h5>Team member-4</h5>
-          </div>
-          <div class="right">
-            <input type="password" />
-            <input type="password" />
-            <input type="password" />
-            <input type="password" />
-          </div>
-        </div>
-        <div class="below2">
-          <div class="teamnumber">
-            <h3>Team - 6</h3>
-          </div>
-          <div class="options2">
-            <div class="icon">
-              <Icon icon="edit" />
-            </div>
-            <div class="icon">
-              <Icon icon="delete" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>-->
-
 <style>
   * {
     margin: 0;
@@ -391,7 +249,6 @@
   .total{
     display:flex;
     flex-direction: row;
-    
   }
   .profile{
     height:84vh;
@@ -463,22 +320,37 @@
     color:black;
     display:flex;
     flex-direction: row;
-    width:32vw;
+    width:25vw;
     background-color: #E9ECFD;
     border-radius:0 0.5em 0.5em 0;
     height:9vh;
   }
+  .team1:hover, .team2:hover,.team3:hover{
+    width:32vw;
+  }
   .number{
     width:25vw;
     height:9vh;
-    
   }
   .links{
+    display: none;
     background-color: #E1FF27;
     width:7vw;
     border-radius:0 0.5em 0.5em 0;
     border-left:0.1rem solid #a6a49d;
   }
+  .team1:hover .links,.team2:hover .links,.team3:hover .links{
+    display: block;
+  }
+  /* #trans{
+    animation-name:transform ;
+    animation-duration: 4s;
+  }
+   @keyframes transform{
+       from{display:none;}
+       to{display:block;}
+    } */
+   
   a{
     font-size:0.9rem;
   }
@@ -494,7 +366,6 @@
     display:flex;
     flex-direction: row;
     width:60vw;
-    /*margin-left:5rem;*/
     border-radius: 1em;
     margin-top:1.5rem; 
   }
@@ -512,8 +383,6 @@
     flex-direction: row;
     width:37vw;
     margin:1em;
-
-    
   }
   .icon{
     background-color: #E1FF27;
@@ -531,7 +400,7 @@
     justify-content: space-around;
     padding-top: 1em;
   }
-  @media only screen and (max-width:1000px){
+  @media(max-width:1000px){
     .total{
       display:flex;
       flex-direction: column;
@@ -542,7 +411,7 @@
         margin:0 1em 2em 0;
       }
       .profile,.team1,.team2,.team3,.name,.email,.phone,.info{
-        width:290px;
+        width:230px;
       }
       .name1,.email1,.phone1,.info1{
         width:100px;
@@ -550,12 +419,11 @@
       .name2,.email2,.phone2,.info2{
         width:135px;
       }
-     
       .number{
         width:230px;
       }
       .links{
-        width:60px;
+        width:87px;
       }
       a{
         font-size:0.7em;
@@ -569,22 +437,22 @@
      .content{
       height:100px;
      }
-  
-     
     }
-@media only screen and (max-width:950px){
+@media(max-width:950px){
   .detail{
     flex-wrap: wrap;
   }
+  .links{
+    display:block;
+  }
 }
-@media only screen and (max-width:700px){
+@media(max-width:700px){
   .content{
     display: flex;
     flex-direction: column;
     justify-content: center;
     height:350px;
     width:390px;
-    /* margin:1.5em; */
   }
   .id{
     width:372px;
@@ -594,14 +462,10 @@
     width:390px;
     border-radius: 0;
   }
-  
 }
-@media only screen and (max-width:560px){
- 
+@media (max-width:560px){
   .content{
     width:300px;
-    
-    
   }
   .icon{
     width:300px;
@@ -610,213 +474,9 @@
     width:284px;
   }
 }
-
-
-  /*.myteams {
-    display: flex;
-    flex-direction: column;
-    padding-bottom: 2em;
-    min-height: 77vh;
-    justify-content: center;
-    align-items: center;
-  }
-  .outer1,
-  .outer2 {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .outer1 {
-    min-height: 50vh;
-  }
-  .outer2 {
-    min-height: 50vh;
-    width: 80vw;
-  }
-  .teams {
-    display: flex;
-    min-height: 100vh;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    padding-bottom: 10vh;
-    background: #eae7dc;
-    gap: 2em;
-  }
-
-  .inner1 {
-    display: flex;
-    flex-direction: column;
-    width: 60vw;
-    height: 20vh;
-    align-items: center;
-    justify-content: center;
-  }
-  .inner2 {
-    display: flex;
-    flex-direction: column;
-    width: 70vw;
-    min-height: 50vh;
-    align-items: center;
-    justify-content: center;
-  }
-  .below1 {
-    width: 45vw;
-    height: 10vh;
-    background: #747272;
-    z-index: 2;
-    margin-top: 1em;
-    margin-right: 1em;
-    border-radius: 1em;
-    transition: all 0.3s ease-in-out;
-  }
-  .below2 {
-    display: flex;
-    width: 61vw;
-    height: 30vh;
-    background: rgb(149, 148, 148);
-    z-index: 2;
-    position: absolute;
-    margin-right: 3.5em;
-    border-radius: 1em;
-    transition: all 0.3s ease-in-out;
-    margin-bottom: 0;
-    align-items: center;
-    padding-inline: 1em;
-    justify-content: space-between;
-  }
-  .header2 {
-    width: 100vw;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-  }
-  .header2 h1 {
-    margin-left: 5vw;
-  }
-  .outer2:hover .below2 {
-    margin-right: -3.5em;
-  }
-  .options1 {
-    display: flex;
-    height: 100%;
-    width: 100%;
-
-    align-items: flex-end;
-    justify-content: flex-start;
-  }
-  .options1 a {
-    padding: 1em;
-  }
-  .options2 {
-    display: flex;
-    flex-direction: column;
-
-    gap: 1em;
-  }
-  .icon {
-    border-radius: 0.3em;
-    height: 2em;
-    width: 2em;
-
-    display: grid;
-    place-items: center;
-    transition: background 0.4s ease;
-  }
-
-  .icon:hover {
-    background: rgb(188, 184, 184);
-    cursor: pointer;
-  }
-
-  .icon:active {
-    transform: translateY(1px);
-  }
-
-  a {
-    color: rgb(0, 0, 0);
-
-    text-decoration: none;
-    font-size: 1em;
-    font-weight: 500;
-  }
-  .left h5,
-  .right input {
-    margin: 0.2em 0;
-    height: 3vw;
-    width: 20vw;
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }
-  a:active {
-    transform: translateY(-2px);
-  }
-  h1 {
-    text-align: center;
-  }
-  .front1 {
-    position: absolute;
-    width: 45vw;
-    height: 10vh;
-    background: #d36767;
-    z-index: 4;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 1em;
-  }
-  .front2 {
-    position: absolute;
-    width: 60vw;
-    height: 30vh;
-    background: #d36767;
-    z-index: 4;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    border-radius: 1em;
-  }
-  .left {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    width: 20vw;
-
-    height: 100%;
-    font-size: calc(0.75em + 1vmin);
-    margin: 0;
-  }
-  .left h5,
-  .right input {
-    height: 2.3vw;
-    text-align: center;
-    margin: 0;
-    border: 0;
-  }
-  .right {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    margin: 0;
-    width: 20vw;
-
-    height: 100%;
-  }
-  .teamnumber h3 {
-    text-orientation: mixed;
-    writing-mode: vertical-rl;
-    transform: rotateZ(180deg);
-  }
-
-  .inner1:hover .below1 {
-    height: 18vh;
-    width: 50vw;
-    margin-left: 1em;
-    margin-top: 2em;
-  }*/
+@media (max-width:380px){
+   .content{
+    margin-left: -2em;
+   }
+}
 </style>
--- src/components/posts_components/Home.svelte
+++ src/components/posts_components/Home.svelte
@@ -27,9 +27,67 @@
     </div>
   </div>
 </div>
+<div class="ideas">
+  <div class="idea">
+    <h2>PROPOSED IDEAS</h2>
+    <div class="table">
+      <div class="name">
+        <div class="number">
+          <h3>Team 6</h3>
+          </div>
+        <div class="proposal">
+          <h3>PROPOSAL-1</h3>
+        </div>
+        <div class="icon">
+          </div>
+      </div>
+        <div class="post">
+          <div class="display">
+          <div class="statement">
+            <h3>PROBLEM STATEMENT</h3>
+          </div>
+          <div class="space">
+          </div>
+          </div>
+          <div class="display">
+          <div class="solution">
+            <h3>PROPOSED SOLUTION</h3>
+          </div>
+          <div class="space">
+          </div>
+          </div>
+          <div class="display">
+          <div class="queries">
+            <h3>QUERIES</h3>
+          </div>
+          <div class="space">
+          </div>
+          </div>
+        </div>
+        <div class="comment">
+          <div class="head">
+          <h3>Comments</h3>
+          </div>
+          <div class="suggestions">
+            <div class="nice">
+              <h3>Nice one</h3>
+              <h4>HOD</h4>
+            </div>
+            <hr>
+            <div class="good">
+              <h3>Good</h3>
+              <h4>Mentor</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+ </div>
+  
 
 <style>
-  .info-card {
+ 
+.info-card {
     background: #6273cb;
     height: 30vh;
     width: 20vw;
@@ -56,7 +114,6 @@
   }
   .inner {
     display: flex;
-
     align-items: center;
   }
   .container {
@@ -78,5 +135,92 @@
   .heading {
     font-weight: 700;
     font-size: medium;
+   } 
+   .table{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+   }
+   .name{
+    display: flex;
+    flex-direction:row;
+    z-index: 1;
+    width:80vw;
+    height:12vh;
+    margin:2em 2em 0 2em;
+    box-shadow: 0 0 10px 0 black;
+    border-radius: 0.5em 0.5em 0 0;
+   }
+   .number{
+    background-color: #6273cb;
+    width:12vw;
+    text-align:center;
+   }
+   .proposal{
+    background-color: #e9ecfd;
+    width:66vw;
+    text-align: center;
+   }
+   .icon{
+    background-color: #e9ecfd;
+    width:12vw;
+   }
+   .post{
+    background-color: #e1ff27;
+    width:80vw;
+    display:flex;
+    flex-direction: column;
   }
+   .display{
+    display: flex;
+    flex-direction: row;
+    width:70vw;
+    align-items: center;
+    margin:0.5em 2em 0 2em;
+   }
+   .statement,.solution,.queries{
+    background-color: #6273cb;
+    border-radius:0.5em 0 0 0.5em;
+    width:15vw;
+    height:14vh;
+    margin:2em 0 2em 2em;
+    padding:0.5em;
+   }
+   .statement,.solution,.queries h3{
+    color:white;
+   }
+   .space{
+    background-color: white;
+    border-radius: 0 0.5em 0.5em 0;
+    width:55vw;
+    height:14vh;
+    margin:2em 0 2em 0;
+    padding:0.5em;
+   }
+   /* h3{
+    font-size: calc(0.75em+1vmin);
+   } */
+   .comment{
+    background-color: #6273cb;
+    display: flex;
+    flex-direction:column;
+    width:80vw;
+   }
+   .nice,.good{
+    display:flex;
+    flex-direction: row;
+    justify-content: space-around;
+   }
+   .head{
+    margin:1em 1em 0 3em;
+   }
+   .suggestions{
+       display:flex;
+       flex-direction: column;
+       margin-left:1em;
+   }
+   hr{
+    border:1.5px solid black;
+    width:80%;
+   }
 </style>
